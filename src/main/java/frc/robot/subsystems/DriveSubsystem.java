e52c2c1cf1ae148cda1e986c0307988fd32566fb
kahraman <kkahraman.akyuzz@gmail.com>
2020-05-07 22:52:34 +0300

    added trajectory

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -13,6 +13,10 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
@@ -33,16 +37,20 @@ public class DriveSubsystem extends SubsystemBase {
   private final Encoder leftWheelEncoder = new Encoder(DriveConstants.leftWhellEncoder_A, DriveConstants.leftWhellEncoder_B, false, EncodingType.k4X);
 
   private final DifferentialDrive m_drive = new DifferentialDrive(leftGroup,rightGroup);
-  
+  private DifferentialDriveOdometry m_odometry;
+
 
   public DriveSubsystem() {
     leftWheelEncoder.setDistancePerPulse(7.62*Math.PI*2.0/2048.0);
     rightWheelencoder.setDistancePerPulse(7.62*Math.PI*2.0/2048.0);
     gyro.calibrate();
+    m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getHeading()));
   }
 
   @Override
   public void periodic() {
+    m_odometry.update(Rotation2d.fromDegrees(getHeading()), leftWheelEncoder.getDistance(),
+                      rightWheelencoder.getDistance());
     // This method will be called once per scheduler run
   }
   
@@ -53,7 +61,23 @@ public class DriveSubsystem extends SubsystemBase {
   public double getHeading(){
    return Math.IEEEremainder(gyro.getAngle(), 360);
   }
+  public double getHeadingReversed(){
+    return Math.IEEEremainder(-1 * gyro.getAngle(), 360);
+
+  }
   public double gettotalDistance(){
     return (leftWheelEncoder.getDistance() + rightWheelencoder.getDistance())/2;
   }
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(leftWheelEncoder.getRate(), rightWheelencoder.getRate());
+  }
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+  }
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+    leftGroup.setVoltage(leftVolts);
+    rightGroup.setVoltage(-rightVolts);
+    m_drive.feed();
+  }
+
 }
